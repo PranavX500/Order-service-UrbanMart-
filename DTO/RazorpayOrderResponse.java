71088e35216132e3e4bb7aaefacea889b295e053
PranavX500 <ps5840432@gmail.com>
2025-12-17 15:45:32 +0530

    RazorpayOrderResponse dto created for mapping the response for Payment-service

---
+++ DTO/RazorpayOrderResponse.java
@@ -0,0 +1,20 @@
+package com.example.Order_Service.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RazorpayOrderResponse {
+    private String razorpayOrderId;
+    private Integer amount;
+    private String currency;
+    private String receipt;
+    private Long orderId;
+    private String requestId;
+
+
+
+}
