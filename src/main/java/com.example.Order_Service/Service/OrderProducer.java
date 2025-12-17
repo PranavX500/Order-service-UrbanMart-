1395aabc0999f10ce11ae97a36d6ebeaf88edf0c
PranavX500 <ps5840432@gmail.com>
2025-12-17 18:16:32 +0530

    Align source layout with standard Spring Boot package structure

---
+++ src/main/java/com.example.Order_Service/Service/OrderProducer.java
@@ -0,0 +1,21 @@
+package com.example.Order_Service.Service;
+
+import com.example.Order_Service.DTO.ProductRequestEvent;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderProducer {
+    private final KafkaTemplate<String , ProductRequestEvent>kafkaTemplate;
+
+    public OrderProducer(KafkaTemplate<String, ProductRequestEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    public void sendIds(ProductRequestEvent event){
+
+        kafkaTemplate.send("Productids",event);
+        System.out.println(" Sent Event to Kafka: " + event);
+    
+        System.out.println(" Sent event to Kafka: " + event);
+    }
+}
